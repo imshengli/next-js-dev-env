a012dae5ddcf1eb7bb6c5e46729ca35480b1a4fa
shengli.zsl <shengli.zsl@alibaba-inc.com>
2017-12-28 13:16:34 +0800

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
-var babel = require("gulp-babel");
+var babel = require('gulp-babel');
 
-gulp.task("default", function () {
-    return gulp.src("src/**/*.js")
+var jsSource = 'src/**/*.js';
+
+gulp.task('compileJS', function () {
+    return gulp.src(jsSource)
         .pipe(babel())
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+    var watcher = gulp.watch(jsSource, ['compileJS']);
+    watcher.on('change', function(event) {
+        console.log(new Date() + ': File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
